97cf6acf919c69ddfecc8559e5414130d7d70520
nima-kam <nimakambarani007@gmail.com>
2020-06-09 10:51:36 +0430

    login window added

---
+++ MC Restaurant/MC Restaurant/customers login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MC_Restaurant
+{
+    /// <summary>
+    /// Interaction logic for customers_login.xaml
+    /// </summary>
+    public partial class customers_login : Window
+    {
+        public customers_login()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Registerbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }     
+
+        private void Confirmbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
