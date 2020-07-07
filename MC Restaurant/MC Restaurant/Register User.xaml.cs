892f030d19e418b0fab21e9ea4adcb3b2573a58b
nima-kam <nimakambarani007@gmail.com>
2020-07-07 15:56:39 +0430

    registeration window UI Completed

--- MC Restaurant/MC Restaurant/Register User.xaml.cs
+++ MC Restaurant/MC Restaurant/Register User.xaml.cs
@@ -23,5 +23,10 @@ namespace MC_Restaurant
         {
             InitializeComponent();
         }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
