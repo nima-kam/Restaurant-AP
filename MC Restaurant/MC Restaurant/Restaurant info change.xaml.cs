e65fe75d0b819c5a39092ae909b53d40cd50ce3e
nima-kam <nimakambarani007@gmail.com>
2020-07-13 23:24:09 +0430

    restaurant establishing completed

--- MC Restaurant/MC Restaurant/Restaurant info change.xaml.cs
+++ MC Restaurant/MC Restaurant/Restaurant info change.xaml.cs
@@ -28,7 +28,64 @@ namespace MC_Restaurant
         {
             try
             {
-                Manager.restaurant = new Restaurant(RestaurantNameBox.Text,ResAddressBox.Text,ResRegionBox.Text,ResPhonenumberBox.Text);
+                if (Restaurant.IsStablished == false)
+                {
+                    if (RestaurantNameBox.Text == "" || ResAddressBox.Text == "" || ResRegionBox.Text == "" || ResPhonenumberBox.Text == "") 
+                    Manager.restaurant = new Restaurant(RestaurantNameBox.Text, ResAddressBox.Text, ResRegionBox.Text, ResPhonenumberBox.Text);
+                }
+                else
+                {
+                    string Name;
+                    if (ResNameCheck.IsChecked == true)
+                    {
+                        if (RestaurantNameBox.Text == "")
+                            throw new Exception("please fill the blank.");
+                       Name =  RestaurantNameBox.Text;
+                    }
+                    else
+                    {
+                       
+                        Name = Manager.restaurant.Name;
+                    }
+                    string address;
+                    if (ResAddressCheck.IsChecked == true)
+                    {
+                        if (ResAddressBox.Text == "")
+                            throw new Exception("please fill the blank.");
+
+                        address = ResAddressBox.Text;
+                    }
+                    else
+                    {
+
+                        address = Manager.restaurant.Address;
+                    }
+                    string region;
+                    if (ResRegionCheck.IsChecked == true)
+                    {
+                        if (ResRegionBox.Text == "")
+                            throw new Exception("please fill the blank.");
+
+                        region = ResRegionBox.Text;
+                    }
+                    else
+                    {
+                        region = Manager.restaurant.Region;
+                    }
+                    string Phone;
+                    if (ResNumberCheck.IsChecked == true)
+                    {
+                        if (ResPhonenumberBox.Text == "")
+                            throw new Exception("please fill the blank.");
+                        Phone = ResPhonenumberBox.Text;
+                    }
+                    else
+                    {
+                        Phone = Manager.restaurant.PhoneNum;
+                    }
+                    Manager.restaurant = new Restaurant(Name,address,region, Phone);
+                    
+                }
                 Managment manager_Login = new Managment();
                 this.Visibility = Visibility.Collapsed;
                 manager_Login.Show();
